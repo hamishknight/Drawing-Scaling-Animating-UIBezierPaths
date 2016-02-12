43d2f75821000dcfdc130433cbf5007328c9c6ad
Hamish Knight <hk@southerncare.co.uk>
2016-02-12 13:22:49 +0000

    Initial Commit

---
+++ Bezier Curving/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bezier Curving
+//
+//  Created by Hamish Knight on 12/02/2016.
+//  Copyright © 2016 Redonkulous Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Bezier CurvingTests/Bezier_CurvingTests.m
@@ -0,0 +1,39 @@
+//
+//  Bezier_CurvingTests.m
+//  Bezier CurvingTests
+//
+//  Created by Hamish Knight on 12/02/2016.
+//  Copyright © 2016 Redonkulous Apps. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Bezier_CurvingTests : XCTestCase
+
+@end
+
+@implementation Bezier_CurvingTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
